bc640d7007fedbd9a3c59e1760442ba77d2f3e06
Storm Mattern <storm.mattern@gmail.com>
2018-07-15 15:59:39 -0500

    Updated the layout for expansion

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+
 int main() 
 {
-	cout << "Welcome to Bulls and Cows" << endl;
+	PrintIntro();
+	PlayGame();
 	return 0;
+}
+
+void PrintIntro()
+{
+	//introduce the game
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls and Cows\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+void PlayGame()
+{
+	//loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+string GetGuess()
+{
+	//get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	return Guess;
 }
